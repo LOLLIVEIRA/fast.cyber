3e5f6a7ed33de881858cb01344900a4351733898
LOLLIVEIRA <los.olliveira@gmail.com>
2026-01-11 22:46:25 -0300

    Configurando export estático

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // ISSO É OBRIGATÓRIO PARA HOSPEDAGEM COMUM
+  images: {
+    unoptimized: true, // Necessário para o comando export funcionar com imagens
+  },
 };
 
 export default nextConfig;
